6a70d953978f173fc53625a587805c753fba1a80
Margus Kerma <margus@amrek.net>
2022-03-30 20:03:55 +0300

    fix: replace godotenv.Marshal with custom write method
    Marshal writes double qoutes around strings which is not
    a good idea, as various other tools (like  kustomize) may
    not properly handle them

--- env.go
+++ env.go
@@ -3,9 +3,13 @@ package main
 import (
 	"fmt"
 	"io"
+	"sort"
+	"strings"
 
 	"github.com/joho/godotenv"
 )
+
+const doubleQuoteSpecialChars = "\\\n\r\"!$`"
 
 type DotEnv struct{}
 
@@ -14,10 +18,30 @@ func (de DotEnv) read(r io.Reader) (map[string]string, error) {
 }
 
 func (de DotEnv) write(w io.Writer, m map[string]string) error {
-	content, err := godotenv.Marshal(m)
-	if err != nil {
-		return err
+	content := make([]string, 0, len(m))
+	for k, v := range m {
+		content = append(content, fmt.Sprintf(`%s=%s`, k, doubleQuoteEscape(v)))
 	}
-	_, err = fmt.Fprintf(w, "%s\n", content)
-	return err
+	sort.Strings(content)
+	for _, v := range content {
+		_, err := fmt.Fprintf(w, "%s\n", v)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func doubleQuoteEscape(line string) string {
+	for _, c := range doubleQuoteSpecialChars {
+		toReplace := "\\" + string(c)
+		if c == '\n' {
+			toReplace = `\n`
+		}
+		if c == '\r' {
+			toReplace = `\r`
+		}
+		line = strings.Replace(line, string(c), toReplace, -1)
+	}
+	return line
 }
